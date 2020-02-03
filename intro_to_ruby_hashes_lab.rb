0dbbf55dbcbd8efa21ce03357fdb736fac5af296
Shervon Ceromain <shervonc@gmail.com>
2020-02-03 18:03:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,8 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  {:key => value}
+  my_hash_creator[:key]
 end
 
 def read_from_hash(hash, key)
